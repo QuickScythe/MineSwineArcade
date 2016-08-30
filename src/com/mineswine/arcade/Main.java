691586239bfafae980da02a158b29ad75f38484a
billwitcher <billwitcher@192.168.1.7>
2016-08-29 21:02:26 -0500

    Alright. Getting there

--- src/com/mineswine/arcade/Main.java
+++ src/com/mineswine/arcade/Main.java
@@ -2,9 +2,18 @@ package com.mineswine.arcade;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mineswine.arcade.utils.Utils;
+
 public class Main extends JavaPlugin {
+	
+	static Main plugin;
+	
 	public void onEnable(){
+		plugin = this;
+		Utils.loadAddons();
 		
 	}
+	
+	public static Main getPlugin(){ return plugin; }
 
 }
---
+++ src/com/mineswine/arcade/utils/Utils.java
@@ -0,0 +1,44 @@
+package com.mineswine.arcade.utils;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.bukkit.Bukkit;
+
+import com.mineswine.arcade.Main;
+
+public class Utils {
+	
+	public static void loadAddons(){
+		File directory = new File(Main.getPlugin().getDataFolder().getPath() + "/addons");
+		Bukkit.broadcastMessage(Main.getPlugin().getDataFolder().getPath() + "/addons");
+		if(!directory.exists())
+			directory.mkdirs();
+		
+		for (File file : directory.listFiles()) {
+		  if (file.isDirectory()) {
+		    continue;
+		  }
+		  if (!file.getName().endsWith(".jar")) {
+		    continue;
+		  }
+
+		  try {
+		    URL url = new URL("jar", "", "file:" + file.getAbsolutePath() + "!/");
+		    URLClassLoader loader = new URLClassLoader(new URL[]{url});
+
+		    Class<?> clazz = loader.loadClass("addon.mineswine.arcade.Main");
+		    Object instance = clazz.newInstance();
+		    Method open = clazz.getMethod("enable");
+		    open.invoke(instance);
+		    
+		    loader.close();
+		  } catch (Exception exception) {
+		    exception.printStackTrace();
+		  }
+		}
+	}
+
+}
